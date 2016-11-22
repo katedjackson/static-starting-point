687184c120f51fa06b206615090e757c2124baa5
C.S. Jiang <christine.s.jiang@gmail.com>
2016-11-21 21:49:19 -0500

    updated image urls for activities and restaurants models

--- yelp.js
+++ yelp.js
@@ -11,9 +11,11 @@ var hotels = ['Hotel Mulberry', 'Andaz Wall Street', 'The Ritz-Carlton New York,
 
 var restos = ['Bouley', 'The Capital Grille', 'Cafe Katja', 'Mas Farmhouse', 'Hop Lee Restaurant', 'Jungsik', 'Rosanjin', 'Xe Lua', 'Tamarind', 'Pylos', 'Bianca Restaurant', 'Marc Forgione', 'Joe\'s Shanghai', 'Rayuela'];
 
-// 'Kittichai',
+// to do manually: 'Kittichai',
 
-var activities = ['Mahayana Temple Buddhist Association', 'South Street Seaport', 'Ground Zero', 'National September 11th Memorial & Museum', 'Battery Park', 'Staten Island Ferry Whitehall Terminal', 'Chinatown Ice Cream Factory', 'Blue Man Group', 'Scott\'s Pizza Tours', 'Apple Store', 'Brooklyn Bridge Park', 'Ellis Island Immigration Museum', 'Washington Square Park', 'Union Square Holiday Market', 'Strand Bookstore'];
+var activities = ['Mahayana Temple Buddhist Association',  'Battery Park', 'Staten Island Ferry Whitehall Terminal', 'Chinatown Ice Cream Factory', 'Blue Man Group', 'Scott\'s Pizza Tours', 'Apple Store', 'Ellis Island Immigration Museum', 'Washington Square Park', 'Union Square Holiday Market'];
+
+// 'South Street Seaport','Ground Zero', 'National September 11th Memorial & Museum', 'Brooklyn Bridge Park', 'Strand Bookstore'
 
 // See http://www.yelp.com/developers/documentation/v2/search_api
 // hotels.forEach(function (hotel) {
@@ -30,40 +32,31 @@ var activities = ['Mahayana Temple Buddhist Association', 'South Street Seaport'
 //   })
 // });
 
-restos.forEach(function (restaurant) {
-  yelp.search({term: restaurant, limit: 1, location: 'New York' })
+// restos.forEach(function (restaurant) {
+//   yelp.search({term: restaurant, limit: 1, location: 'New York' })
+//   .then(function (data) {
+//     console.log(data.businesses[0].name);
+//     large_image_url = data.businesses[0].image_url.replace(/ms\.jpg/, 'l.jpg');
+//     Restaurant.findOne({where: {name: restaurant}})
+//     .then(function (foundRestaurant) {
+//       foundRestaurant.update({image: large_image_url})
+//       .then(function (updatedRestaurant) {
+//         console.log(`Updated image url for ${restaurant}! Hooray!`);
+//       })
+//     })
+//   })
+// });
+
+activities.forEach(function (activity) {
+  yelp.search({term: activity, limit: 1, location: 'New York' })
   .then(function (data) {
-    console.log(data.businesses[0].name);
-    // large_image_url = data.businesses[0].image_url.replace(/ms\.jpg/, 'l.jpg');
-    // Hotel.findOne({where: {name: hotel}})
-    // .then(function (foundHotel) {
-    //   foundHotel.update({image: large_image_url})
-    //   .then(function (updatedHotel) {
-    //     console.log(`Updated image url for ${hotel}! Hooray!`);
-    //   })
-    // })
+    large_image_url = data.businesses[0].image_url.replace(/ms\.jpg/, 'l.jpg');
+    Activity.findOne({where: {name: activity}})
+    .then(function (foundActivity) {
+      foundActivity.update({image: large_image_url})
+      .then(function (updatedActivity) {
+        console.log(`Updated image url for ${activity}! Hooray!`);
+      })
+    })
   })
 });
-
-// yelp.search({ term: 'food', location: 'Montreal' })
-// .then(function (data) {
-//   console.log(data);
-// })
-// .catch(function (err) {
-//   console.error(err);
-// });
-
-// // See http://www.yelp.com/developers/documentation/v2/business
-// yelp.business('yelp-san-francisco')
-//   .then(console.log)
-//   .catch(console.error);
-
-// yelp.phoneSearch({ phone: '+15555555555' })
-//   .then(console.log)
-//   .catch(console.error);
-
-// // A callback based API is also available:
-// yelp.business('yelp-san-francisco', function(err, data) {
-//   if (err) return console.log(error);
-//   console.log(data);
-// });
