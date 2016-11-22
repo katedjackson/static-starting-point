183c2334c9253c9003b85d8451e12a49166e9761
Kate <katelynj29@gmail.com>
2016-11-22 14:12:34 -0500

    itinerary.js functionality added

--- public/itinerary.js
+++ public/itinerary.js
@@ -1,5 +1,15 @@
-function allowDrop() {
+function allowDrop(ev) {
+    ev.preventDefault();
+}
 
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("itinerary");
+    ev.target.appendChild(document.getElementById(data));
 }
 
 $(document).ready(function() {
